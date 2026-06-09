e899b2a61ccc4cd720b963dd0723e9ab62623a60
hwarange <asdf78964@naver.com>
2026-06-09 17:50:08 +0900

    backend 기초설정

---
+++ backend/src/main/java/com/syncshopper/SyncShopperApplication.java
@@ -0,0 +1,13 @@
+package com.syncshopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncShopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncShopperApplication.class, args);
+	}
+
+}
